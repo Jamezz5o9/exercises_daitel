152870168c4ef13e760f82ec5a4ed80d3585db62
James <adulojujames@gmail.com>
2022-11-08 06:44:33 +0100

    thank God

---
+++ src/Chapter17/Exercise15.java
@@ -0,0 +1,37 @@
+package Chapter17;
+
+public class Exercise15 {
+    private int list;
+    private int scores;
+    private int age;
+
+    public Exercise15(int list, int scores, int age) {
+        this.list = list;
+        this.scores = scores;
+        this.age = age;
+    }
+
+    public int getList() {
+        return list;
+    }
+
+    public void setList(int list) {
+        this.list = list;
+    }
+
+    public int getScores() {
+        return scores;
+    }
+
+    public void setScores(int scores) {
+        this.scores = scores;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
